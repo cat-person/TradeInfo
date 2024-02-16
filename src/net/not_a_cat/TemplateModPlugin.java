acd33456864bd2bcd778ff07572bb62456c03d80
Aleksei Klyshnniov <alexey.klyshnikov@gmail.com>
2024-02-16 23:17:29 +0400

    Experiments with adding stuff to intel (API is bit cumbersome)

--- src/net/not_a_cat/TemplateModPlugin.java
+++ src/net/not_a_cat/TemplateModPlugin.java
@@ -4,24 +4,16 @@ import com.fs.starfarer.api.BaseModPlugin;
 import com.fs.starfarer.api.Global;
 
 public class TemplateModPlugin extends BaseModPlugin {
-    @Override
-    public void onApplicationLoad() throws Exception {
-        super.onApplicationLoad();
 
-        // Test that the .jar is loaded and working, using the most obnoxious way possible.
-        throw new RuntimeException("Template mod loaded! Remove this crash in TemplateModPlugin.");
+    @Override
+    public void onGameLoad(boolean newGame) {
+        super.onGameLoad(newGame);
+        addIntel();
     }
 
-    @Override
-    public void onNewGame() {
-        super.onNewGame();
+    private void addIntel() {
+        TradeIntelItem intel = new TradeIntelItem();
 
-        // The code below requires that Nexerelin is added as a library (not a dependency, it's only needed to compile the mod).
-//        boolean isNexerelinEnabled = Global.getSettings().getModManager().isModEnabled("nexerelin");
-
-//        if (!isNexerelinEnabled || SectorManager.getManager().isCorvusMode()) {
-//                    new MySectorGen().generate(Global.getSector());
-        // Add code that creates a new star system (will only run if Nexerelin's Random (corvus) mode is disabled).
-//        }
+        Global.getSector().getIntelManager().addIntel(intel);
     }
 }
